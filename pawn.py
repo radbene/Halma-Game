4fc6e6b94c550b6ad66fb068d36a3680f4c19c97
radbene <benedykcinskiradoslaw@gmail.com>
2025-04-16 19:58:39 +0200

    deleted pawn

--- pawn.py
+++
@@ -1,8 +0,0 @@
-class Pawn():
-    def __init__(self, row: int, col: int, player: str):
-        self.row: int = row
-        self.col: int = col
-        self.player: str = player
-
-    def __str__(self):
-        return self.player
